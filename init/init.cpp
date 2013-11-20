be5d92d7f8ee7279b9b9c25850d2ce4f822cd06b
David Greaves <david@dgreaves.com>
2013-11-20 19:18:51 +0000

    (hybris) Add a ready trigger to init to run post boot
    Change-Id: I9c828463424c0e82c3de0159db08299e7ce6fe06

--- init/init.cpp
+++ init/init.cpp
@@ -694,6 +694,12 @@ int main(int argc, char** argv) {
     // Run all property triggers based on current state of the properties.
     am.QueueBuiltinAction(queue_property_triggers_action, "queue_property_triggers");
 
+    /* run all device triggers based on current state of device nodes in /dev */
+    am.QueueBuiltinAction(queue_device_triggers_action, "queue_device_triggers");
+
+    /* Run actions when all boot up is done and init is ready */
+    am.QueueEventTrigger("ready");
+
     while (true) {
         if (!waiting_for_exec) {
             am.ExecuteOneCommand();
